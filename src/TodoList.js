1dbb4a0c15b4b0ad7c1635ecb8a0b246086118da
unknown <krishagarwal.ka21@gmail.com>
2023-09-02 14:49:08 +0530

    First Commit

---
+++ src/TodoList.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function TodoList() {
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState('');
+
+    useEffect(() => {
+        axios.get('/api/tasks/')
+            .then(response => setTasks(response.data))
+            .catch(error => console.error(error));
+    }, []);
+
+    // ***************************************ADD A TASK************************************************************************
+
+    const handleAddTask = () => {
+        if (newTask.trim() === '') return;
+
+        axios.post('http://127.0.0.1:8000/api/tasks/', { title: newTask, completed: false })
+            .then(() => {
+                setNewTask('');
+                loadTasks();
+            })
+            .catch(error => console.error(error));
+    };
+
+    // *********************************************LOAD ALL TASKS**************************************************************
+
+    const loadTasks = () => {
+        axios.get('http://127.0.0.1:8000/api/tasks/')
+            .then(response => setTasks(response.data))
+            .catch(error => console.error(error));
+    };
+
+    // ******************************************MARK A TASK AS COMPLETE**********************************************************
+
+    const handleToggleComplete = (taskId, completed) => {
+        axios.patch(`http://127.0.0.1:8000/api/tasks/${taskId}/`, { completed })
+            .then(() => loadTasks())
+            .catch(error => console.error(error));
+    };
+
+    // ******************************************DELETE A TASKS*******************************************************************
+
+    const handleDeleteTask = (taskId) => {
+        axios.delete(`http://127.0.0.1:8000/api/tasks/${taskId}/`)
+            .then(() => loadTasks())
+            .catch(error => console.error(error));
+    };
+
+    // *************************************************PAGE DISPLAY*************************************************************
+
+    return (
+        <div>
+            <h1>To-Do List</h1>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Add a new task"
+                    value={newTask}
+                    onChange={(e) => setNewTask(e.target.value)}
+                />
+                <button onClick={handleAddTask}>Add</button>
+            </div>
+            <div className='container'>
+            <ul >
+                {tasks.map((task) => (
+                    <li key={task.id}>
+                        <input
+                            type="checkbox"
+                            checked={task.completed}
+                            onChange={() => handleToggleComplete(task.id, !task.completed)}
+                        />
+                        <span className={task.completed ? 'completed' : ''}>{task.title}</span>
+                        <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+            </div>
+
+            
+        </div>
+    );
+}
+
+export default TodoList;
